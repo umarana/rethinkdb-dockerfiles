1b845d7ac881e3aeaebe9cdfe0dcd92481f527f7
Anastas Dancha <anapsix@random.io>
2015-07-20 15:05:35 -0400

    TUTUM: use TUTUM_SERVICE_HOSTNAME

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 if [ "${1:0:1}" = '-' ]; then
   [ -n "$TUTUM_CONTAINER_FQDN" ] && TUTUM_ARGS+=( --canonical-address $TUTUM_CONTAINER_FQDN )
   [ -n "$TUTUM_IP_ADDRESS" ] && TUTUM_ARGS+=( --canonical-address ${TUTUM_IP_ADDRESS%/*} )
-  if [ -n "$TUTUM_SERVICE_FQDN" ]; then
-    for host in $(host -s -t a $TUTUM_SERVICE_FQDN | awk '{print $NF}'); do
+  if [ -n "$TUTUM_SERVICE_HOSTNAME" ]; then
+    for host in $(host -s -t a $TUTUM_SERVICE_HOSTNAME | awk '{print $NF}'); do
       TUTUM_ARGS+=( --join $host )
     done
   fi
